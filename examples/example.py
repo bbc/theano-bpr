f81c02cfa1a4879af3cccb812537bbdc8a0138a8
Yves Raimond <yves.raimond@bbc.co.uk>
2014-05-06 17:46:35 +0100

    Making the example script a command line script

--- examples/example.py
+++ examples/example.py
@@ -1,14 +1,21 @@
+#!/usr/bin/env python
+
 from theano_bpr.utils import load_data_from_csv
 from theano_bpr import BPR
+import sys
+
+if len(sys.argv) == 2:
+    print "Usage: ./example.py training_data.csv testing_data.csv"
+    sys.exit(0)
 
 # Loading train data
-train_data, users_to_index, items_to_index = load_data_from_csv('week/training.csv')
+train_data, users_to_index, items_to_index = load_data_from_csv(sys.argv[1])
 # Loading test data
-test_data, users_to_index, items_to_index = load_data_from_csv('week/testing.csv', users_to_index, items_to_index)
+test_data, users_to_index, items_to_index = load_data_from_csv(sys.argv[2], users_to_index, items_to_index)
 
-# Initialising BPR model
+# Initialising BPR model, 10 latent factors
 bpr = BPR(10, len(users_to_index.keys()), len(items_to_index.keys()))
-# Training model
+# Training model, 10 epochs
 bpr.train(train_data, epochs=10)
 # Testing model
 print bpr.test(test_data)
