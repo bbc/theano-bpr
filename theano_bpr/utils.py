10586f4766a51df96a834a1f98d80343dfd95a29
Yves Raimond <yves.raimond@bbc.co.uk>
2014-05-08 15:10:14 +0100

    Adding an example ipython notebook, using movielens

--- theano_bpr/utils.py
+++ theano_bpr/utils.py
@@ -15,6 +15,7 @@
 # limitations under the License.
 
 from collections import defaultdict
+import urllib
 
 def load_data_from_csv(csv, users_to_i = {}, items_to_i = {}):
     """
@@ -33,6 +34,48 @@ def load_data_from_csv(csv, users_to_i = {}, items_to_i = {}):
       of (user, item) tuples, a mapping from user ids to integers
       and a mapping from item ids to integers.
     """
+    raw_data = []
+    with open(csv) as f:
+        for line in f.readlines():
+            user, item = line.strip().split(',')
+            raw_data.append((user, item))
+    return load_data_from_array(raw_data, users_to_i, items_to_i)
+
+def load_data_from_movielens(url, threshold, users_to_i = {}, items_to_i = {}):
+    """
+      Loads movielens data from a URL, e.g.
+
+        http://files.grouplens.org/datasets/movielens/ml-100k/
+
+      Initial mappings from user and item identifiers
+      to integers can be passed using `users_to_i`
+      and `items_to_i` respectively.
+
+      This function will return a data array consisting
+      of (user, item) tuples, a mapping from user ids to integers
+      and a mapping from item ids to integers.
+    """
+    raw_data = []
+    for line in urllib.urlopen(url).readlines():
+        user, item, rating, timestamp = line.split('\t')
+        if int(rating) > threshold:
+            raw_data.append((user, item))
+    return load_data_from_array(raw_data)
+
+def load_data_from_array(array, users_to_i = {}, items_to_i = {}):
+    """
+      Loads data from an array of tuples of the form:
+
+          (user_id, item_id)
+
+      Initial mappings from user and item identifiers
+      to integers can be passed using `users_to_i`
+      and `items_to_i` respectively.
+
+      This function will return a data array consisting
+      of (user, item) tuples, a mapping from user ids to integers
+      and a mapping from item ids to integers.
+    """
     data = []
     if len(users_to_i.values()) > 0:
         u = max(users_to_i.values()) + 1
@@ -42,14 +85,13 @@ def load_data_from_csv(csv, users_to_i = {}, items_to_i = {}):
         i = max(items_to_i.values()) + 1
     else:
         i = 0
-    with open(csv) as f:
-        for line in f.readlines():
-            user, item = line.strip().split(',')
-            if not users_to_i.has_key(user):
-                users_to_i[user] = u
-                u += 1
-            if not items_to_i.has_key(item):
-                items_to_i[item] = i
-                i += 1
-            data.append((users_to_i[user], items_to_i[item]))
+    for user, item in array:
+        if not users_to_i.has_key(user):
+            users_to_i[user] = u
+            u += 1
+        if not items_to_i.has_key(item):
+            items_to_i[item] = i
+            i += 1
+        data.append((users_to_i[user], items_to_i[item]))
     return data, users_to_i, items_to_i
+
